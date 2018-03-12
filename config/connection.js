86497c5ab88a64380d806d2a3be287238820449c
Frank Valdez <fvaldez421@gmail.com>
2018-03-12 10:06:06 -0700

    update

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 // var connection = mysql.createConnection({
 //   host: "localhost",
 //   user: "root",
-//   password: "CMMAnum_1",
+//   password: "",
 //   database: "hotdogs_db"
 // });
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,11 @@
 
-USE hotdogs_db;
-
 INSERT INTO hotdogs (hotdog_name, devoured) 
-VALUES ("Frank", 0);
+VALUES ("JohnDog", 0);
+INSERT INTO hotdogs (hotdog_name, devoured) 
+VALUES ("DanielDog", 0);
+INSERT INTO hotdogs (hotdog_name, devoured) 
+VALUES ("JasonDog", 0);
+INSERT INTO hotdogs (hotdog_name, devoured) 
+VALUES ("AndyDog", 1);
+INSERT INTO hotdogs (hotdog_name, devoured) 
+VALUES ("SamDog", 1);
