79ccdd3d76a260422cab1039ffe36fc992d8a2c7
Frank Valdez <fvaldez421@gmail.com>
2018-04-25 15:46:58 -0700

    prettier

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,5 @@
 var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "hotdogs_db"
-// });
-
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-
 var connection;
 if(process.env.JAWSDB_URL) {
   //Heroku deployment
@@ -26,8 +10,8 @@ if(process.env.JAWSDB_URL) {
         root: 3000,
         host: "localhost",
         user: "root",
-        password: "",
-        database: "db_name",
+        password: "CMMAnum_1",
+        database: "hotdogs_db",
     });
 };
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-var mysql = require("mysql");
 var express = require("express");
-var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var port = process.env.PORT || 3000;
 
-var routes = require("./controllers/hotdogs_controller.js");
 var app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main", extname: ".handlebars"}));
 app.set("view engine", "handlebars");
+
+var routes = require("./controllers/hotdogs_controller.js");
 
 app.use("/", routes);
 
