e6896679af4e5aecbe7b94cdd54b4d1213a3e9cf
Frank Valdez <fvaldez421@gmail.com>
2018-05-29 21:49:13 -0700

    update

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/hotdogs_controller.js");
 var port = process.env.PORT || 3000;
-
 var app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main", extname: ".handlebars"}));
 app.set("view engine", "handlebars");
-
-var routes = require("./controllers/hotdogs_controller.js");
 
 app.use("/", routes);
 
